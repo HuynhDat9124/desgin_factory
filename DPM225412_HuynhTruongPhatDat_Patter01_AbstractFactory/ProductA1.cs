edc2e1b2fc30ae643c0e7680c19ee6b12d2df251
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-09 10:36:57 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Patter01_AbstractFactory/ProductA1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225412_HuynhTruongPhatDat_Patter01_AbstractFactory
+{
+    class ProductA1 : AbstractProductA
+    {
+    }
+}
