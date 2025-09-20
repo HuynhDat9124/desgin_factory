ae2371433dc0916a3752a27cb2935b7158538a12
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-20 22:08:16 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Pattern05_Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DPM225412_HuynhTruongPhatDat_Pattern05_Singleton
+{
+    /// <summary>
+    /// Singleton Design Pattern - Client
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Constructor is protected -- cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DPM225412_HuynhTruongPhatDat_Pattern05_Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225412_HuynhTruongPhatDat_Pattern05_Singleton
+{
+    /// <summary>
+    /// The 'Singleton' class
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        // Constructor is 'protected'
+        protected Singleton()
+        {
+        }
+
+        public static Singleton Instance()
+        {
+            // Uses lazy initialization.
+            // Note: this is not thread safe.
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+}
