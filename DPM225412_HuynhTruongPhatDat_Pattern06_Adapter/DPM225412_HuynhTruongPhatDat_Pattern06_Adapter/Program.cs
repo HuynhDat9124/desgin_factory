a871fe96deb9300dce616d43331f074f7408a8e8
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-20 22:09:06 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Pattern06_Adapter/DPM225412_HuynhTruongPhatDat_Pattern06_Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DPM225412_HuynhTruongPhatDat_Pattern06_Adapter
+{
+    /// <summary>
+    /// Adapter Design Pattern - Client
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create adapter and place a request
+            Target target = new Adapter();
+            target.Request();
+
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DPM225412_HuynhTruongPhatDat_Pattern06_Adapter/DPM225412_HuynhTruongPhatDat_Pattern06_Adapter/Target.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225412_HuynhTruongPhatDat_Pattern06_Adapter
+{
+    /// <summary>
+    /// The 'Target' class
+    /// </summary>
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("Called Target Request()");
+        }
+    }
+}
