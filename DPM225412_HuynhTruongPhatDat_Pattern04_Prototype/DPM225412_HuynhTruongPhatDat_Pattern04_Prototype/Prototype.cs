620224069e78b3297c7b3add6de8a24e0de6eec5
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-20 22:02:44 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Pattern04_Prototype/DPM225412_HuynhTruongPhatDat_Pattern04_Prototype/Prototype.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225412_HuynhTruongPhatDat_Pattern04_Prototype
+{
+    /// <summary>
+    /// The 'Prototype' abstract class
+    /// </summary>
+    public abstract class Prototype
+    {
+        private string id;
+
+        // Constructor
+        public Prototype(string id)
+        {
+            this.id = id;
+        }
+
+        // Gets id
+        public string Id
+        {
+            get { return id; }
+        }
+
+        public abstract Prototype Clone();
+    }
+}
