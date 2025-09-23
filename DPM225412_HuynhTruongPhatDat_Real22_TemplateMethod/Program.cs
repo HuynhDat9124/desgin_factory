c5f029e8469ce16908db764c8ff62b34182b5a10
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-23 10:11:06 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Real22_TemplateMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Template.RealWorld
+{
+    /// <summary>
+    /// Template Design Pattern
+    /// </summary>
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataAccessor categories = new Categories();
+            categories.Run(5);
+
+            DataAccessor products = new Products();
+            products.Run(3);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
