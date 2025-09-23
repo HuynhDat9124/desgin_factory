649f81034b6d4463b181287a76272ec23caba421
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-23 10:02:33 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Real17_Mediator/AbstractChatroom.cs
@@ -0,0 +1,13 @@
+﻿namespace Mediator.RealWorld
+{
+    /// <summary>
+    /// The 'Mediator' abstract class
+    /// </summary>
+
+    public abstract class AbstractChatroom
+    {
+        public abstract void Register(Participant participant);
+        public abstract void Send(
+            string from, string to, string message);
+    }
+}
