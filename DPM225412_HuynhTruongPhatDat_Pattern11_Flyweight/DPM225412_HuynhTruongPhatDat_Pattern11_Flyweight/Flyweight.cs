a0a118ee5ca6dd6857a00d543a8c8c4616cc198c
Huynh Dat <dat_dpm225412@student.agu.edu.vn>
2025-09-23 07:30:16 +0700

    Add files via upload

---
+++ DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight/DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight/Flyweight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight
+{
+    public abstract class Flyweight
+    {
+        public abstract void Operation(int extrinsicstate);
+    }
+    /// <summary>
+    /// The 'ConcreteFlyweight' class
+    /// </summary>
+    public class ConcreteFlyweight : Flyweight
+    {
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("ConcreteFlyweight: " + extrinsicstate);
+        }
+    }
+}
---
+++ DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight/DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight/Program.cs
@@ -0,0 +1,28 @@
+﻿using DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight;
+using System;
+using System.Collections.Generic;
+namespace DPM225412_HuynhTruongPhatDat_Pattern11_Flyweight
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int extrinsicstate = 22;
+            FlyweightFactory factory = new FlyweightFactory();
+            
+            Flyweight fx = factory.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+            Flyweight fy = factory.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+            Flyweight fz = factory.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+            UnsharedConcreteFlyweight fu = new
+                UnsharedConcreteFlyweight();
+            fu.Operation(--extrinsicstate);
+            
+            Console.ReadKey();
+        }
+    }
+}
